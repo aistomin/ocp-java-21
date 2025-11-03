13a22273c94cc5d3a0f6dc84ee72df688e8cdee1
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-03 13:50:31 +0100

    Playing around with object initialisation.

---
+++ chapter1/objects_initialisation/Chick.java
@@ -0,0 +1,13 @@
+public class Chick {
+    int numEggs = 12;
+    String name;
+    // int complex = a + b;
+    int a = 2;
+    int b = 3;
+    int complex = a + b; 
+    public Chick() {
+        System.out.println("Chick. Constructor.");
+        name = "Duke";
+    }
+}
+

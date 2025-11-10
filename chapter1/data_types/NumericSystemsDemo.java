8436eb10c3dd03cc899ae22d1c53405cdffcfad0
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-10 12:02:27 +0100

    Chapter 1. Playing with the numeric system declarations.

---
+++ chapter1/data_types/NumericSystemsDemo.java
@@ -0,0 +1,26 @@
+public class NumericSystemsDemo {
+    
+    public static void main(String[] args) {
+        int o = 017;
+        System.out.println("Octal example: " + o);
+
+        int xd1 = 0xFF;
+        System.out.println("Hexadecimal example: " + xd1);
+
+        int xd2 = 0xff;
+        System.out.println("Hexadecimal example: " + xd2);
+
+        int xd3 = 0XFf;
+        System.out.println("Hexadecimal example: " + xd3);
+
+        int xd4 = 0XfF;
+        System.out.println("Hexadecimal example: " + xd4);
+
+        int b1 = 0b10;
+        System.out.println("Hexadecimal example: " + b1);
+
+        int b2 = 0B10;
+        System.out.println("Hexadecimal example: " + b2);  
+    }
+}
+

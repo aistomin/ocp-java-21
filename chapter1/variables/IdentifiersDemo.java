7c173f01a4309695290dc374afe585fba16d3b5f
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-11 08:07:38 +0100

    Chapter 1. Playing with variable names.

---
+++ chapter1/variables/IdentifiersDemo.java
@@ -0,0 +1,24 @@
+public class IdentifiersDemo {
+    public static void main(String[] args) {
+        // int 3a = 1; - wrong!
+        int a3 = 1;
+        System.out.println("a3 = " + a3);
+
+        int _3a = 2;
+        System.out.println("_3a = " + _3a);
+
+        // int _ = 3; - wrong!
+        int __ = 3;
+        System.out.println("__ = " + __);
+        
+        int $ = 4;
+        System.out.println("$ = " + $);
+
+        int $1 = 5;
+        System.out.println("$1 = " + $1);
+
+        // int for = 6; - wrong!
+        int foR = 6;
+        System.out.println("foR = " + foR);
+    }
+}

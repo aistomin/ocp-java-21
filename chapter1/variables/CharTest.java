4e53320fd2b06f7b0cb0a85e2d19e49510291590
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-11 08:32:45 +0100

    Chapter 1. Testing how char works..

---
+++ chapter1/variables/CharTest.java
@@ -0,0 +1,12 @@
+public class CharTest {
+
+    private char ch;
+
+    private void printCh() {
+        System.out.println("ch = {" + ch + "}");
+    }
+
+    public static void main(String[] args) {
+        new CharTest().printCh();
+    }
+}

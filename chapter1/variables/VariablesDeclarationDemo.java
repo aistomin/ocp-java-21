3730c53db0f3224569c9f51990a2fab808042d62
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-11 08:18:29 +0100

    Chapter 1. Playing with multiple variables' declaration.

---
+++ chapter1/variables/VariablesDeclarationDemo.java
@@ -0,0 +1,8 @@
+public class VariablesDeclarationDemo {
+    public static void main(String[] args) {
+        int a, b;
+        int c = 1, d = 2;
+        int e, f = 3;
+        int g = 4, h, i = 5;
+    }
+}

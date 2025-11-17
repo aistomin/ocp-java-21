b36e48e6b406ce44b2e320e50c7047240e5b539a
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-17 17:08:16 +0100

    Chapter 2. Playing with operators.

---
+++ chapter2/PlayingAround.java
@@ -0,0 +1,13 @@
+public class PlayingAround {
+    public static void main(String arg[]) {
+        var a = -5;
+        var b = +a;
+        System.out.println("b = " + b);
+
+        System.out.println("x = y = 5");
+        var x = 5;
+        System.out.println("++x " + ++x);
+        var y = 5;
+        System.out.println("y++ " + y++);
+    }
+}

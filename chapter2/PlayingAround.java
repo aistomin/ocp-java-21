42edcc056e822a8bd16a738388ec0be4f30bb0b2
aistomin <andrej.istomin.ikeen@gmail.com>
2025-11-18 10:24:28 +0100

    Chapter 2. Playing with assignments.

--- chapter2/PlayingAround.java
+++ chapter2/PlayingAround.java
@@ -9,5 +9,19 @@ public class PlayingAround {
         System.out.println("++x " + ++x);
         var y = 5;
         System.out.println("y++ " + y++);
+
+        float f = 10;
+        System.out.println("f = " + f);
+
+        double d1 = 10;
+        System.out.println("d1 = " + d1);
+
+        double d2 = 100_000_000_000L;
+        System.out.println("d2 = " + d2);
+
+        short s1 = 32_000 + 700;
+        System.out.println("s1 = " + s1);
+
+        // short s2 = 32_000 + 800; - fails
     }
 }
